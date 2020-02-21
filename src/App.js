01054802b68d1fa97a14b2be2bd1f30ca4958d26
Gu234 <guciofilipek@gmail.com>
2020-02-21 21:19:23 +0100

    fetch and first three columns

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// fetch('https://recruitment.hal.skygate.io/companies')
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((myJson) => {
+//     console.log(myJson);
+//   });
+
+// fetch('https://recruitment.hal.skygate.io/incomes/1')
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((myJson) => {
+//     console.log(myJson);
+//   });
+class App extends Component {
+
+  state = {
+    dataFetched: false,
+    companies: []
+  }
+
+  componentDidMount() {
+    if (!this.state.dataFetched) {
+
+      fetch('https://recruitment.hal.skygate.io/companies')
+        .then((response) => {
+          return response.json();
+        })
+        .then((companies) => {
+          this.setState({ companies, dataFetched: true })
+          console.log(companies);
+        });
+    }
+  }
+
+  companiesWithIncomes() {
+    return this.state.companies
+  }
+
+  render() {
+    return (
+      <>
+        <table>
+          <thead>
+            <th>Id</th>
+            <th>Name</th>
+            <th>City</th>
+            <th>Total Income</th>
+            <th>Avarage Income</th>
+            <th>Last month income</th>
+          </thead>
+          <tbody>
+            {this.companiesWithIncomes().map(company =>
+              <tr key={company.id}>
+                <td>{company.id}</td>
+                <td>{company.name}</td>
+                <td>{company.city}</td>
+                <td>{company.totalIncome}</td>
+                <td>{company.avarageIncome}</td>
+                <td>{company.lastMontIncome}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </>
+    );
+  }
 }
 
 export default App;
