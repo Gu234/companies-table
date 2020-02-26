f4cd900018c83c279e351eeb5728e3415b0ad410
Gu234 <guciofilipek@gmail.com>
2020-02-26 07:12:16 +0100

    Updates

---
+++ precache-manifest.8ac4cdd6084f620e09158c8763f7d0c4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f116a7136cd7ef0c55bc13a1f950c79",
+    "url": "/companies-table/index.html"
+  },
+  {
+    "revision": "560aa8596be2844e71ea",
+    "url": "/companies-table/static/css/main.540292bb.chunk.css"
+  },
+  {
+    "revision": "bed122cb7e74931487ce",
+    "url": "/companies-table/static/js/2.f8b1164c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/companies-table/static/js/2.f8b1164c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "560aa8596be2844e71ea",
+    "url": "/companies-table/static/js/main.9aa7982a.chunk.js"
+  },
+  {
+    "revision": "79497f0c885e1cbee048",
+    "url": "/companies-table/static/js/runtime-main.619b67fe.js"
+  },
+  {
+    "revision": "c951b3bff3e346d34f27b325dcc68cd0",
+    "url": "/companies-table/static/media/begin-arrow.c951b3bf.svg"
+  },
+  {
+    "revision": "f1a073cff1238a58e9074a7dc1a7853d",
+    "url": "/companies-table/static/media/end-arrow.f1a073cf.svg"
+  },
+  {
+    "revision": "95bad0cdd13b4294e83bd832c32e5657",
+    "url": "/companies-table/static/media/left-arrow.95bad0cd.svg"
+  },
+  {
+    "revision": "3eb545aa7c9568276dfb65bdd601e431",
+    "url": "/companies-table/static/media/right-arrow.3eb545aa.svg"
+  },
+  {
+    "revision": "e6b947fad8346b4bb9ec8c5ddc677bbe",
+    "url": "/companies-table/static/media/search.e6b947fa.svg"
+  }
+]);
